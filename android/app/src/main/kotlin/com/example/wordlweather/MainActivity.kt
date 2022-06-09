4c2b29299d624ab01592ac393eb9c261f6591d0d
Khalid Ali <khalidchaudry130@gmail.com>
2022-06-09 07:03:06 +0500

    weather update

---
+++ android/app/src/main/kotlin/com/example/wordlweather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wordlweather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    debugPrint(position.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location'),
+      ),
+      body: Column(
+        children: [
+          ActionChip(
+              label: const Text('Get Location'),
+              onPressed: () => getLocation()),
+        ],
+      ),
+    );
+  }
+}
